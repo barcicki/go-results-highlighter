75381011ac26b120155e2b944e14eee962374ba0
barcicki <barcicki@gmail.com>
2016-08-13 22:42:54 +0200

    Reorganized index so the elements have jQuery wrapper if jQuery is present

--- src/index.js
+++ src/index.js
@@ -5,25 +5,31 @@ import { DOM_ATTRIBUTES } from './lib/settings';
 import { asArray } from './lib/utils';
 
 function initialize() {
-    asArray(document.querySelectorAll(`[${DOM_ATTRIBUTES.RESULT_TABLE}]`))
-        .forEach((tableEl) => new GoResultsHighlighter(tableEl));
+    const elementsWithResults = document.querySelectorAll(`[${DOM_ATTRIBUTES.RESULT_TABLE}]`);
+
+    if (typeof jQuery !== 'undefined') {
+
+        jQuery.fn.goResultsHighlighter = function (options) {
+            this.each((index, element) => {
+                const highlighter = new GoResultsHighlighter(element, options);
+
+                $(highlighter.element).data('GoResultsHighlighter', highlighter);
+            });
+            return this;
+        };
+
+        jQuery(elementsWithResults).goResultsHighlighter();
+
+    } else {
+        asArray(elementsWithResults)
+            .forEach(tableEl => new GoResultsHighlighter(tableEl));
+    }
 }
 
 if (document.readyState === 'complete') {
     initialize();
 } else {
     document.addEventListener('DOMContentLoaded', initialize, false);
-}
-
-if (typeof jQuery !== 'undefined') {
-    jQuery.fn.goResultsHighlighter = function (options) {
-        this.each(function (index, element) {
-            let highlighter = new GoResultsHighlighter(element, options);
-
-            $(highlighter.element).data('GoResultsHighlighter', highlighter);
-        });
-        return this;
-    };
 }
 
 export default GoResultsHighlighter;
