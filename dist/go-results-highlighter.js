78d2ad08355d386105804a652c90eadbeee2997a
barcicki <barcicki@gmail.com>
2016-10-02 18:42:43 +0200

    Improved cooperation with jQuery - registers plugin before ready event if possible

--- dist/go-results-highlighter.js
+++ dist/go-results-highlighter.js
@@ -11,19 +11,31 @@ var _utils = require('./lib/utils');
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+/**
+ * Registers goResultsHighlighter plugin in jQuery
+ */
+function registerJQueryPlugin() {
+    jQuery.fn.goResultsHighlighter = function jQueryHighlighterWrapper(options) {
+        this.each(function (index, element) {
+            var highlighter = new _wrapper2.default(element, options);
+
+            $(highlighter.element).data('GoResultsHighlighter', highlighter);
+        });
+        return this;
+    };
+}
+
+/**
+ * Runs the highlighter on existing tables
+ */
 function initialize() {
     var elementsWithResults = document.querySelectorAll('[' + _settings.DOM_ATTRIBUTES.RESULT_TABLE + ']');
 
     if (typeof jQuery !== 'undefined') {
 
-        jQuery.fn.goResultsHighlighter = function (options) {
-            this.each(function (index, element) {
-                var highlighter = new _wrapper2.default(element, options);
-
-                $(highlighter.element).data('GoResultsHighlighter', highlighter);
-            });
-            return this;
-        };
+        if (!jQuery.fn.goResultsHighlighter) {
+            registerJQueryPlugin();
+        }
 
         jQuery(elementsWithResults).goResultsHighlighter();
     } else {
@@ -33,9 +45,19 @@ function initialize() {
     }
 }
 
+// if the website is already fully loaded proceed with initialisation
 if (document.readyState === 'complete') {
     initialize();
+
+    // otherwise ...
 } else {
+
+    // check for jQuery and register the plugin at first to make sure that it will be available
+    // before any $(document).ready calls
+    if (typeof jQuery !== 'undefined') {
+        registerJQueryPlugin();
+    }
+
     document.addEventListener('DOMContentLoaded', initialize, false);
 }
 
--- src/index.js
+++ src/index.js
@@ -1,22 +1,34 @@
 'use strict';
 
 import GoResultsHighlighter from './lib/wrapper';
-import { DOM_ATTRIBUTES } from './lib/settings';
-import { asArray } from './lib/utils';
+import {DOM_ATTRIBUTES} from './lib/settings';
+import {asArray} from './lib/utils';
 
+/**
+ * Registers goResultsHighlighter plugin in jQuery
+ */
+function registerJQueryPlugin() {
+    jQuery.fn.goResultsHighlighter = function jQueryHighlighterWrapper(options) {
+        this.each((index, element) => {
+            const highlighter = new GoResultsHighlighter(element, options);
+
+            $(highlighter.element).data('GoResultsHighlighter', highlighter);
+        });
+        return this;
+    };
+}
+
+/**
+ * Runs the highlighter on existing tables
+ */
 function initialize() {
     const elementsWithResults = document.querySelectorAll(`[${DOM_ATTRIBUTES.RESULT_TABLE}]`);
 
     if (typeof jQuery !== 'undefined') {
 
-        jQuery.fn.goResultsHighlighter = function (options) {
-            this.each((index, element) => {
-                const highlighter = new GoResultsHighlighter(element, options);
-
-                $(highlighter.element).data('GoResultsHighlighter', highlighter);
-            });
-            return this;
-        };
+        if (!jQuery.fn.goResultsHighlighter) {
+            registerJQueryPlugin();
+        }
 
         jQuery(elementsWithResults).goResultsHighlighter();
 
@@ -26,9 +38,19 @@ function initialize() {
     }
 }
 
+// if the website is already fully loaded proceed with initialisation
 if (document.readyState === 'complete') {
     initialize();
+
+// otherwise ...
 } else {
+
+    // check for jQuery and register the plugin at first to make sure that it will be available
+    // before any $(document).ready calls
+    if (typeof jQuery !== 'undefined') {
+        registerJQueryPlugin();
+    }
+
     document.addEventListener('DOMContentLoaded', initialize, false);
 }
 
